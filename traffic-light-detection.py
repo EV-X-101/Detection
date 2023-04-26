2566963803a44dab16b8513b6aa9429efe188ca3
Yonathan Assefa <yab95assefa@gmail.com>
2023-04-26 15:11:49 +0300

    Update traffic-light-detection.py
    This code will recognize the traffic light using color segmentation.

--- traffic-light-detection.py
+++ traffic-light-detection.py
@@ -1,5 +1,6 @@
 import cv2
 import torch
+import numpy as np
 
 # Load a pre-trained YOLOv5 model (e.g., YOLOv5s)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
@@ -28,7 +29,36 @@ while True:
         x_min, y_min, x_max, y_max, confidence, class_idx = item
         class_name = results.names[int(class_idx)]
         if class_name == 'traffic light':
-            print(f"Class: {class_name}, Confidence: {confidence:.2f}")
+            # Extract the region of interest (ROI) around the traffic light
+            roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+
+            # Convert the ROI to HSV color space
+            hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+            # Define the color ranges for red, yellow, and green lights
+            lower_red = np.array([0, 50, 50])
+            upper_red = np.array([10, 255, 255])
+            lower_yellow = np.array([20, 100, 100])
+            upper_yellow = np.array([30, 255, 255])
+            lower_green = np.array([50, 100, 100])
+            upper_green = np.array([70, 255, 255])
+
+            # Apply color segmentation to the ROI to determine the color of the traffic light
+            mask_red = cv2.inRange(hsv_roi, lower_red, upper_red)
+            mask_yellow = cv2.inRange(hsv_roi, lower_yellow, upper_yellow)
+            mask_green = cv2.inRange(hsv_roi, lower_green, upper_green)
+            red_pixels = cv2.countNonZero(mask_red)
+            yellow_pixels = cv2.countNonZero(mask_yellow)
+            green_pixels = cv2.countNonZero(mask_green)
+            color = 'unknown'
+            if red_pixels > yellow_pixels and red_pixels > green_pixels:
+                color = 'red'
+            elif yellow_pixels > red_pixels and yellow_pixels > green_pixels:
+                color = 'yellow'
+            elif green_pixels > red_pixels and green_pixels > yellow_pixels:
+                color = 'green'
+            
+            print(f"Traffic Light: Color: {color}, Confidence: {confidence:.2f}")
             print(f"Bounding Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
             cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
 
@@ -39,6 +69,4 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+# Release the
