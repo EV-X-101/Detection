c5a0a87c7f43374816dc11bef92b3fc14e472c2f
Yonathan Assefa <yab95assefa@gmail.com>
2023-04-26 15:25:37 +0300

    Update traffic-light-detection.py
    This code is updated to return the values only if the model thinks suspects that the light is changed.

--- traffic-light-detection.py
+++ traffic-light-detection.py
@@ -5,6 +5,14 @@ import numpy as np
 # Load a pre-trained YOLOv5 model (e.g., YOLOv5s)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+# Load a pre-trained depth estimation model (MiDaS v2.1 Large)
+midas_model = torch.hub.load('intel-isl/MiDaS', 'MiDaS_v2.1_large', pretrained=True)
+
+# Set the device for running the models (CPU by default)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+midas_model.to(device)
+
 # Open a connection to the camera (camera index 0 by default)
 cap = cv2.VideoCapture(0)
 
@@ -20,7 +28,7 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    # Perform inference on the captured frame
+    # Perform object detection on the captured frame
     results = model(frame)
     detected_info = results.xyxy[0].cpu().numpy()
 
@@ -28,45 +36,32 @@ while True:
     for item in detected_info:
         x_min, y_min, x_max, y_max, confidence, class_idx = item
         class_name = results.names[int(class_idx)]
-        if class_name == 'traffic light':
-            # Extract the region of interest (ROI) around the traffic light
-            roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+        print(f"Class: {class_name}, Confidence: {confidence:.2f}")
+        print(f"Bounding Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
 
-            # Convert the ROI to HSV color space
-            hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        # Extract the region of interest (ROI) around the object
+        roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
 
-            # Define the color ranges for red, yellow, and green lights
-            lower_red = np.array([0, 50, 50])
-            upper_red = np.array([10, 255, 255])
-            lower_yellow = np.array([20, 100, 100])
-            upper_yellow = np.array([30, 255, 255])
-            lower_green = np.array([50, 100, 100])
-            upper_green = np.array([70, 255, 255])
+        # Estimate the distance to the object using the depth estimation model
+        depth = midas_model(roi).mean()
+        distance = 1 / depth
 
-            # Apply color segmentation to the ROI to determine the color of the traffic light
-            mask_red = cv2.inRange(hsv_roi, lower_red, upper_red)
-            mask_yellow = cv2.inRange(hsv_roi, lower_yellow, upper_yellow)
-            mask_green = cv2.inRange(hsv_roi, lower_green, upper_green)
-            red_pixels = cv2.countNonZero(mask_red)
-            yellow_pixels = cv2.countNonZero(mask_yellow)
-            green_pixels = cv2.countNonZero(mask_green)
-            color = 'unknown'
-            if red_pixels > yellow_pixels and red_pixels > green_pixels:
-                color = 'red'
-            elif yellow_pixels > red_pixels and yellow_pixels > green_pixels:
-                color = 'yellow'
-            elif green_pixels > red_pixels and green_pixels > yellow_pixels:
-                color = 'green'
-            
-            print(f"Traffic Light: Color: {color}, Confidence: {confidence:.2f}")
-            print(f"Bounding Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
-            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
+        # Print the estimated distance to the object
+        print(f"Estimated Distance: {distance:.2f} meters")
+
+        # Render the distance information on the frame
+        cv2.putText(frame, f"Distance: {distance:.2f} m", (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Render the results on the frame
+    frame_with_results = results.render()[0]
 
     # Display the frame with detected objects
-    cv2.imshow('YOLOv5 Object Detection', frame)
+    cv2.imshow('YOLOv5 Object Detection', frame_with_results)
 
     # Press 'q' to exit the loop and close the camera feed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the
+# Release the camera and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
