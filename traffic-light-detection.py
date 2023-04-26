7873796b2bc479be93b4063961f07c09263db2d2
Yonathan Assefa <yab95assefa@gmail.com>
2023-04-26 14:59:41 +0300

    Create traffic-light-detection.py
    This code detects traffic light, but do not work traffic light recognition

---
+++ traffic-light-detection.py
@@ -0,0 +1,44 @@
+import cv2
+import torch
+
+# Load a pre-trained YOLOv5 model (e.g., YOLOv5s)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Open a connection to the camera (camera index 0 by default)
+cap = cv2.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+# Loop to continuously capture frames from the camera
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    # Perform inference on the captured frame
+    results = model(frame)
+    detected_info = results.xyxy[0].cpu().numpy()
+
+    # Display detected information
+    for item in detected_info:
+        x_min, y_min, x_max, y_max, confidence, class_idx = item
+        class_name = results.names[int(class_idx)]
+        if class_name == 'traffic light':
+            print(f"Class: {class_name}, Confidence: {confidence:.2f}")
+            print(f"Bounding Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
+            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
+
+    # Display the frame with detected objects
+    cv2.imshow('YOLOv5 Object Detection', frame)
+
+    # Press 'q' to exit the loop and close the camera feed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
