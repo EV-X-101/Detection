518ad6f9049ed9e189c9b1e0b75a6ffedd9b35e4
Yonathan Assefa <yab95assefa@gmail.com>
2023-05-04 11:27:14 +0300

    feat: added traffic light detection
    I added traffic detection to the current object detection. But this detects one color at a traffic light. Meaning that, If two colors at the same time turned on for example green and yellow and red and yellow, it would probably be confused.

--- object-detection.py
+++ object-detection.py
@@ -9,6 +9,9 @@ RPI_IP_ADDRESS = os.environ.get('RPI_IP_ADDRESS')
 # Load a pre-trained YOLOv5 model (e.g., YOLOv5s)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+# Load a pre-trained depth estimation model (MiDaS v2.1 Large)
+midas_model = torch.hub.load('intel-isl/MiDaS', 'MiDaS_v2.1_large', pretrained=True)
+
 # Define camera calibration matrix and distortion coefficients
 K = np.array([[5.9421434211923245e+02, 0., 3.1950000000000000e+02],
               [0., 5.9421434211923245e+02, 2.3950000000000000e+02],
@@ -33,6 +36,7 @@ if not cap.isOpened():
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((RPI_IP_ADDRESS, 5000)) # Replace with Raspberry Pi IP address
 print('Socket connection established')
+
 # Loop to continuously capture frames from the camera
 while True:
     ret, frame = cap.read()
@@ -40,7 +44,7 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    # Perform inference on the captured frame
+    # Perform object detection on the captured frame
     results = model(frame)
     detected_info = results.xyxy[0].cpu().numpy()
 
@@ -52,31 +56,61 @@ while True:
         x_min, y_min, x_max, y_max, confidence, class_idx = item
         class_name = results.names[int(class_idx)]
         
-        # Estimate the distance using stereo vision formula
-        xl = x_min
-        yl = y_min
-        xr = x_max
-        yr = y_max
-        d = abs(xl - xr)
-        depth = (b * f) / d
+        # Extract the region of interest (ROI) around the object
+        roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
         
-        # Display the object name and estimated depth
-        text = f"{class_name} {depth:.2f}m"
+        # Estimate the distance to the object using the depth estimation model
+        depth = midas_model(roi).mean()
+        distance = 1 / depth
+
+        # Display the object name and estimated distance
+        text = f"{class_name} {distance:.2f}m"
         cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
         cv2.putText(frame, text, (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
         print(f"Detected: {text}")
-        
+
         # Check if an obstacle is detected and send signal to Raspberry Pi
         for threat_class in threat_classes:
-            if class_name == threat_class and depth < threat_distances[threat_class]:
+            if class_name == threat_class and distance < threat_distances[threat_class]:
                 obstacle_detected = True
                 print(f"{threat_class.capitalize()} detected. Stopping car.")
                 sock.sendall(b'stop')
                 break
 
-    # Send signal to Raspberry Pi to move car forward if no obstacle is detected
+        # Check if the object is a traffic light
+        if class_name == 'traffic light':
+            # Extract the region of interest (ROI) for the traffic light
+            traffic_light_roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+
+            # Convert the ROI to HSV color space
+            hsv_roi = cv2.cvtColor(traffic_light_roi, cv2.COLOR_BGR2HSV)
+
+            # Threshold the image to extract the color of the traffic light
+            lower_red = np.array([0, 100, 100])
+            upper_red = np.array([10, 255, 255])
+            mask1 = cv2.inRange(hsv_roi, lower_red, upper_red)
+
+            lower_red = np.array([170, 100, 100])
+            upper_red = np.array([180, 255, 255])
+            mask2 = cv2.inRange(hsv_roi, lower_red, upper_red)
+
+            mask = mask1 + mask2
+            color = 'red' if cv2.countNonZero(mask) > 0 else 'green'
+
+            # Render the traffic light color on the frame
+            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 0, 255), 2)
+            cv2.putText(frame, f"Color: {color}", (int(x_min), int(y_min) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (0, 0, 255), 2)
+
+            # Check if the traffic light is red and send signal to Raspberry Pi to stop the car
+            if color == 'red':
+                print("Red traffic light detected. Stopping car.")
+                sock.sendall(b'stop')
+                obstacle_detected = True
+
+    # Send signal to Raspberry Pi to move car forward if no obstacle or red traffic light is detected
     if not obstacle_detected:
-        print("No obstacle detected. Moving car forward.")
+        print("No obstacle or red traffic light detected. Moving car forward.")
         sock.sendall(b'forward')
 
     # Display the frame with detected objects
