f2e16c5daf7bc5d6127fa03489d7de9dc4dece51
Yonathan Assefa <yab95assefa@gmail.com>
2023-05-05 22:00:03 +0300

    Update test-flight-E.py

--- test-flight-E.py
+++ test-flight-E.py
@@ -92,8 +92,8 @@ while True:
     detected_info = results.xyxy[0].cpu().numpy()
 
     # Check if an obstacle is detected and send signal to Raspberry Pi
-    threat_classes = ['person', 'car', 'truck', 'bus', 'chair'] # Replace with the classes you want to detect
-    threat_distances = {'person': 0.5, 'car': 3.0, 'truck': 5.0, 'bus': 8.0, 'chair': 0.6} # Replace with the distances you want to detect each class at
+    threat_classes = ['person', 'car', 'truck', 'bus'] # Replace with the classes you want to detect
+    threat_distances = {'person': 0.5, 'car': 3.0, 'truck': 5.0, 'bus': 8.0} # Replace with the distances you want to detect each class at
     obstacle_detected = False
     for item in detected_info:
         x_min, y_min, x_max, y_max, confidence, class_idx = item
