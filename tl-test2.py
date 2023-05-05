76f15f287aff4792fe40cb404cf2fc11aec7e7d6
Yonathan Assefa <yab95assefa@gmail.com>
2023-05-05 17:36:58 +0300

    Update tl-test2.py

--- tl-test2.py
+++ tl-test2.py
@@ -1,123 +1,201 @@
+import os
+import time
 import torch
+import numpy as np
 import cv2
+from PIL import Image
+from pathlib import Path
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import (
+    check_img_size, non_max_suppression, scale_coords, xyxy2xywh)
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+from core.utils import utils as deep_utils
+from core.utils import yolov5_utils
+from core.config import cfg
+from core.yolov5 import filter_boxes
+from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
 
-from models.experimental import attempt_load
-from utils.datasets import letterbox
-from utils.general import check_img_size, non_max_suppression, scale_coords
-from utils.plots import plot_one_box
+def traffic_light_state(frame, bbox):
+    xmin, ymin, xmax, ymax = [int(coord) for coord in bbox]
+    cropped_frame = frame[ymin:ymax, xmin:xmax]
 
+    if cropped_frame.size == 0:
+        print("Empty cropped frame!")
+        return "Unknown"
 
-# Load the pre-trained YOLOv5 model for traffic sign and traffic light detection
-model_path = 'models/yolov5s_traffic.pt'
-model = attempt_load(model_path, map_location=torch.device('cpu'))
-model.eval()
+    # Convert the cropped image to BGR and then to HSV color space
+    bgr_frame = cv2.cvtColor(cropped_frame, cv2.COLOR_RGB2BGR)
+    hsv_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2HSV)
 
-# Define the class names for the traffic signs and traffic lights that the model can detect
-class_names = ['traffic_light', 'stop_sign', 'yield_sign', 'speed_limit_sign']
+    # Define color ranges in HSV
+    red_lower_range1 = np.array([0, 70, 50])
+    red_upper_range1 = np.array([10, 255, 255])
 
-# Define the color ranges for the traffic lights in HSV color space
-red_lower_range1 = np.array([0, 70, 50])
-red_upper_range1 = np.array([10, 255, 255])
+    red_lower_range2 = np.array([160, 70, 50])
+    red_upper_range2 = np.array([180, 255, 255])
 
-red_lower_range2 = np.array([160, 70, 50])
-red_upper_range2 = np.array([180, 255, 255])
+    yellow_lower_range = np.array([20, 100, 100])
+    yellow_upper_range = np.array([30, 255, 255])
 
-yellow_lower_range = np.array([20, 100, 100])
-yellow_upper_range = np.array([30, 255, 255])
+    green_lower_range = np.array([40, 70, 70])
+    green_upper_range = np.array([90, 255, 255])
 
-green_lower_range = np.array([40, 70, 70])
-green_upper_range = np.array([90, 255, 255])
+    # Create masks for each color range
+    red_mask1 = cv2.inRange(hsv_frame, red_lower_range1, red_upper_range1)
+    red_mask2 = cv2.inRange(hsv_frame, red_lower_range2, red_upper_range2)
+    red_mask = cv2.addWeighted(red_mask1, 1.0, red_mask2, 1.0, 0.0)
 
-# Open a connection to the camera (camera index 0 by default)
-cap = cv2.VideoCapture(0)
+    yellow_mask = cv2.inRange(hsv_frame, yellow_lower_range, yellow_upper_range)
+    green_mask = cv2.inRange(hsv_frame, green_lower_range, green_upper_range)
 
-# Check if the camera opened successfully
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-    exit()
+    # Count the number of pixels for each color
+    red_count = cv2.countNonZero(red_mask)
+    yellow_count = cv2.countNonZero(yellow_mask)
+    green_count = cv2.countNonZero(green_mask)
 
-# Loop over the frames captured from the camera and perform traffic sign and traffic light detection on each frame
-while True:
-    # Capture a frame from the camera
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Could not read frame.")
-        break
+    # If yellow pixels are detected, prioritize yellow color
+    if yellow_count > 10:
+        return "Yellow"
 
-    # Resize and pad the frame to the input size of the YOLOv5 model
-    img_size = 640
-    img = letterbox(frame, new_shape=img_size)[0]
+    # Determine the traffic light state based on the number of colored pixels
+    color_counts = [("Red", red_count), ("Green", green_count)]
+    color_counts.sort(key=lambda x: x[1], reverse=True)
+    return color_counts[0][0]
 
-    # Convert the frame to RGB color space
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def main(save=False, info=False, count_objects=False):
+    # Definition of the parameters
+    max_cosine_distance = 0.4
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+    # initialize deep sort
+    model_filename = 'deep_sort_model/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    # calculate cosine distance metric
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    # initialize tracker
+    tracker = Tracker(metric)
 
-    # Convert the frame to a PyTorch tensor
-    img = torch.from_numpy(img).to(torch.device('cpu'))
-    img = img.permute(2, 0, 1).unsqueeze(0).float()
+    # load configuration for object detector
+    weights_path = 'yolov5s.pt'
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = torch.load(weights_path, map_location=device)['model'].float()  # load to FP32
+    model.to(device).eval()
 
-    # Perform object detection on the frame using the YOLOv5 model
-    results = model(img)
+    # get video ready to save locally if flag is set
+    if save:
+        # by default VideoCapture returns float instead of int
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        codec = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('', codec, fps, (width, height))
 
-    # Get the detected object information
-    detected_info = results.xyxy[0].cpu().numpy()
+    frame_num = 0
+    # while video is running
+    while True:
+        ret, frame = cap.read()
 
-    # Loop over the detected objects and perform traffic sign and traffic light detection on each object
-    for item in detected_info:
-        x_min, y_min, x_max, y_max, confidence, class_idx = item
-        class_name = class_names[int(class_idx)]
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+        else:
+            print('Video has ended!')
+            break
 
-        # Extract the region of interest (ROI) around the object
-        roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+        frame_num +=1
 
-        # Check if the object is a traffic light
-        if class_name == 'traffic_light':
-            # Extract the region of interest (ROI) for the traffic light
-            traffic_light_roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+        frame_size = frame.shape[:2]
+        img = cv2.resize(frame, (640, 640))
+        img = torch.from_numpy(img).to(device)
+        img = img.permute(2, 0, 1).float().unsqueeze(0) / 255.0
 
-            # Convert the ROI to HSV color space
-            hsv_roi = cv2.cvtColor(traffic_light_roi, cv2.COLOR_BGR2HSV)
-            
-            # Threshold the image to extract the color of the traffic light
-            mask1 = cv2.inRange(hsv_roi, red_lower_range1, red_upper_range1)
-            mask2 = cv2.inRange(hsv_roi, red_lower_range2, red_upper_range2)
-            red_mask = mask1 + mask2
+        start_time = time.time()
 
-            yellow_mask = cv2.inRange(hsv_roi, yellow_lower_range, yellow_upper_range)
+        # run detection
+        pred = model(img, augment=False)[0]
+        pred = non_max_suppression(pred, conf_thres=0.5, iou_thres=0.45)
 
-            green_mask = cv2.inRange(hsv_roi, green_lower_range, green_upper_range)
+        # convert detections to deep sort format
+        detections = []
+        for i, det in enumerate(pred):
+            if len(det):
+                det = det[:, :4]
+                det[:, 0] *= frame_size[1] / 640
+                det[:, 1] *= frame_size[0] / 640
+                det[:, 2] *= frame_size[1] / 640
+                det[:, 3] *= frame_size[0] / 640
+                conf = det[:, -1]
+                if len(det) > 1:
+                    det = np.array([det[np.argmax(conf)]])
+                for *xyxy, conf in det:
+                    detections.append(Detection(xyxy, conf, "person", encoder(frame, xyxy)))
 
-            # Determine the color of the traffic light
-            if cv2.countNonZero(red_mask) > 10:
-                traffic_light_color = 'red'
-            elif cv2.countNonZero(yellow_mask) > 10:
-                traffic_light_color = 'yellow'
-            elif cv2.countNonZero(green_mask) > 10:
-                traffic_light_color = 'green'
-            else:
-                traffic_light_color = 'unknown'
+        # initialize color map
+        cmap = plt.get_cmap('tab20b')
+        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
-            # Render the traffic light color on the frame
-            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 0, 255), 2)
-            cv2.putText(frame, f"Traffic light: {traffic_light_color}", (int(x_min), int(y_min) - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # run non-maxima suppression
+        boxs = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        classes = np.array([d.class_name for d in detections])
+        indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]       
 
-        # Check if the object is a traffic sign
-        elif class_name in ['stop_sign', 'yield_sign', 'speed_limit_sign']:
-            # Render the traffic sign label on the frame
-            label = class_name.replace('_', ' ').capitalize()
-            cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 0, 255), 2)
-            cv2.putText(frame, label, (int(x_min), int(y_min) - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
 
-    # Display the resulting frame
-    cv2.imshow('Traffic sign and traffic light detection', frame)
+        # update tracks
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue 
+            bbox = track.to_tlbr()
+            class_name = track.get_class()
 
-    # Exit if the user presses the 'q' key
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
 
-# Release the capture and destroy all windows
-cap.release()
-cv2.destroyAllWindows()
+            # Check if the detected object is a traffic light and get its state
+            if class_name == "traffic light":
+                light_state = traffic_light_state(frame, bbox)
+                class_name = f"{class_name} ({light_state})"
+                if light_state == "Red":
+                    color = (255,0,0)
+                elif light_state =="Yellow":
+                    color = (181, 176, 20)
+                elif light_state == "Green":
+                    color = (35, 120, 18)
+
+            # draw bbox on screen
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+            cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+
+            # if enable info flag then print details about each track
+            if info:
+                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+
+        # calculate frames per second of running detections
+        fps = 1.0 / (time.time() - start_time)
+
+        result = np.asarray(frame)
+        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        
+        cv2.imshow("Output Video", result)
+        
+        # if output flag is set, save video file
+        if save:
+            out.write(result)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+
+    cv2.destroyAllWindows()
