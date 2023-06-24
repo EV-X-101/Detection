e00adf88dfb1e8c2808e9990183e6f0ddef21121
Yonathan Assefa <yab95assefa@gmail.com>
2023-06-24 12:11:12 +0300

    updated the path to give maneuvers also from the mapbox API

--- gps/path.py
+++ gps/path.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 import requests
-import polyline
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,7 +15,7 @@ destination = '8.557840,39.287434'  # Example: New York, NY
 current_location = '8.563830,39.289224'  # Example: 37.7749,-122.4194
 
 # Construct the API request URL
-url = f'https://api.mapbox.com/directions/v5/mapbox/driving/{current_location};{destination}?access_token={api_key}'
+url = f'https://api.mapbox.com/directions/v5/mapbox/driving/{current_location};{destination}?alternatives=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token={api_key}'
 
 # Send the API request
 response = requests.get(url)
@@ -25,18 +24,22 @@ response = requests.get(url)
 if response.status_code == 200:
     # Extract the route data from the response
     data = response.json()
-    
+
     # Get the distance and duration of the route
     distance = data['routes'][0]['distance']
     duration = data['routes'][0]['duration']
-    
-    # Extract the polyline coordinates
-    polyline_data = data['routes'][0]['geometry']
-    path = polyline.decode(polyline_data)
-    
-    # Print the distance, duration, and path coordinates
+
+    # Extract the maneuver instructions and bearing for each step
+    steps = data['routes'][0]['legs'][0]['steps']
+    maneuvers = [step['maneuver']['instruction'] for step in steps]
+    bearings = [step['maneuver']['bearing_after'] for step in steps]
+
+    # Print the distance, duration, maneuver instructions, and bearings
     print('Distance:', distance, 'meters')
     print('Duration:', duration, 'seconds')
-    print('Path:', path)
+    print('Maneuvers:')
+    for i, (maneuver, bearing) in enumerate(zip(maneuvers, bearings)):
+        print(f"Step {i + 1}: {maneuver} (Bearing: {bearing})")
+
 else:
     print('Error:', response.status_code)
