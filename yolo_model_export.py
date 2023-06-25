ffc5ff8c0b6305fad6241f51add8b0c28bc64431
Yonathan Assefa <yab95assefa@gmail.com>
2023-06-25 17:37:34 +0300

    Update yolo_model_export.py

--- yolo_model_export.py
+++ yolo_model_export.py
@@ -3,12 +3,14 @@ import torch
 weights_path = './models/non/best_93.pt'
 output_path = './models/exported/traffic_sign_detection.pt'
 
+# Define input size
+input_height = 224
+input_width = 224
+
 # Load the pre-trained model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path)
 
 # Export the model
-torch.onnx.export(model.model, torch.zeros(1, 3, model.model.hyp['height'], model.model.hyp['width']),
-                  output_path, verbose=False)
-
+torch.onnx.export(model, torch.zeros(1, 3, input_height, input_width), output_path, verbose=False)
 # Save the exported model in the desired format
 model.model.save(output_path)
